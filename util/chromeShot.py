30f894ef6053411fd9adf8c78bf0917e16202718
CBHue <CBHue@users.noreply.github.com>
2020-07-13 12:44:08 -0400

    Add files via upload

---
+++ util/chromeShot.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def chromeShot (url,f):
+	chrome_options = Options()
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--window-size=1920x1080")
+	chrome_options.add_argument("--no-sandbox")
+	chrome_options.add_argument("--user-data-dir /tmp")
+	chrome_options.add_argument('--ignore-certificate-errors')
+
+	chrome_driver = "/usr/bin/chromedriver"
+
+	try:
+		driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+		driver.set_page_load_timeout(3)
+		driver.get(url)
+		driver.get_screenshot_as_file(f)
+		driver.quit()
+	except Exception as e:
+		print("screenshot Error:" + str(e), "debug")
---
+++ util/hostCheck.py
@@ -0,0 +1,122 @@
+import os
+import re
+import socket
+import ipaddress
+
+from nslookup import Nslookup
+import dns
+
+import util.osWork 
+import util.helper as helper
+
+def resolveHName (hName):
+	ip = "NXDOMAIN"
+
+	try:
+		result = dns.resolver.query(hName, 'A')
+		for ipval in result:
+			ip = ipval.to_text()
+	except Exception as e:
+		ip = "NXDOMAIN"
+
+	return str(ip) 
+
+def validateHost (network):
+	hostMAP = {}
+
+	helper.printW("Validating 	: " +  network)
+	cidr = ""
+
+	# is this a ip/cidr
+	match = re.search(r'(\d+.\d+.\d+.\d+)(/\d+)', network)
+	if match:
+		matchWork = match.group(1)
+		helper.printW("IP Addr: " + matchWork)
+		helper.printW("Subnet : " + match.group(2))
+		cidr = match.group(2)
+
+		# is it one host or more
+		if match.group(2) == '/32':
+			if ipaddress.ip_address(matchWork).is_private:
+				helper.printR("Private Network ... Skipping")
+				return
+			
+			ip = confirmIP(matchWork, cidr)
+			if not ip:
+				return
+			hostMAP[ip] = network
+
+		else:
+			if ipaddress.ip_address(matchWork).is_private:
+				helper.printR("Private Network ... Skipping")
+				return
+			
+			helper.printY("Expanding Network: " + network)
+			expandedhostFile = ipaddress.ip_network(network)
+			cidr = "/32"
+			for ip in expandedhostFile:
+				confirmIP(ip, cidr)
+				helper.printW("Fin		: " + str(ip))
+				print('')
+				# This is if u have a rate limit for API calls
+				helper.printW("Sleeping for a second ...")
+				time.sleep( 2 )
+				print('')
+	
+	# its not a network/cidr so its either a single IP or Hostname
+	else:
+		cidr = "/32"
+		match = re.search(r'(\d+.\d+.\d+.\d+)', network)
+		if match:
+			helper.printG("Single IP 	: " + network)
+
+			matchWork = network	
+			ip = confirmIP(matchWork, cidr)
+			if not ip:
+				return
+			# Lets see if we can do a reverse lookup
+			name = network
+			try:
+				n = socket.gethostbyaddr(network)
+				name = n[0]
+			except Exception as e:
+				name = network
+
+			hostMAP[ip] = name
+
+		# Must be a hostname
+		else:
+			cidr = "/32"
+			helper.printW("Hostname 	: " + network)
+			sip = resolveHName(network)
+
+			if sip is "NXDOMAIN":
+				helper.printR("No IP found : " + network)
+				return
+
+			helper.printG("Resolved IP : " + sip)
+			ip = confirmIP(sip, cidr)
+			if ip:
+				hostMAP.update({ip : network})
+
+	return hostMAP
+
+def confirmIP (matchWork, cidr):	
+	# Lets see if this is a real IP
+	ipFull = str(matchWork) + cidr
+	matchWork = str(matchWork)
+
+	try:
+		ip = ipaddress.ip_address(matchWork)
+	except ValueError:
+		helper.printR("Address/Netmask is invalid: "+ ipFull)
+		return False
+	except Exception as e:
+		helper.printR("[validateHost] " + str(e) + " " + ipFull)
+		return False
+	if ipaddress.ip_address(matchWork).is_private:
+		helper.printR("Address/Netmask is Private: "+ ipFull)
+		return False
+		
+	# If we got here the IP is valid.
+	return matchWork
